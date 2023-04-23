706d69f89cc4c1cdc905c794ad95a0568e54909a
LightBurdenOfficial <francis.santiago@lightburden.net>
2023-04-23 16:35:20 -0300

    [NEW] Separação do header para melhor dinamismo entre as páginas

---
+++ public/partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="title" content="Duo Dominion">
+		<meta name="description" content="O Duo Dominion é um jogo de tabuleiros que consiste em uma batalha num mapa selecionado pela maioria da equipe.">
+		<meta name="keywords" content="duo,duo dominion, dominio, multiplayer, coops, co-operation, two players, team, teams, game, jogo, tabuleiro, brasileiro">
+		<meta name="robots" content="index, follow">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="language" content="Portuguese">
+		<meta name="revisit-after" content="5 days">
+		<meta name="author" content="Francis Santiago">
+		<title>Duo Dominion</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+		<link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+	</head>
